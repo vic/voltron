52abd94276093b3658145b4fd33b5a5482945de5
Victor Borja <vborja@apache.org>
2016-05-01 09:36:41 -0500

    better message ^^

--- lib/mix/tasks/YouAndMe.ex
+++ lib/mix/tasks/YouAndMe.ex
@@ -4,13 +4,13 @@ defmodule Mix.Tasks.YouAndMe do
   @moduledoc "Lets map-reduce you and me on an infinite loop"
   @shortdoc "Lets map-reduce you and me on an infinite loop"
   def run(_) do
-    you_and_me
+    you_and_me("You", "Me")
   end
 
   # This is private, just between you and me
-  defp you_and_me do
-    IO.puts "Lets mix together, you and me"
-    you_and_me
+  defp you_and_me(you, me) do
+    IO.puts "Lets mix together, #{you} and #{me}. Happy one-year-of-functional-love"
+    you_and_me(me, you)
   end
 
 end
